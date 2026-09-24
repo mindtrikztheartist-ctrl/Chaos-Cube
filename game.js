5ee0594178494ce73511db870fc74985945f9bfd
mindtrikztheartist-ctrl <mindtrikztheartist@gmail.com>
2026-09-23 21:43:45 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,30 @@
-function startGame(){
-  document.getElementById("message").textContent = 
-    "Your journey has begun!";
+let enemyHP = 50;
+
+function attack() {
+    enemyHP -= 10;
+  
+    if (enemyHP <=0) {
+        enemyHP = 0;
+        document.getElementById("message").textContent =
+            "Enemy defeated!";
+    } else {
+        document.getElementById("message").textContent =
+            "You attacked the enemy! Enemy HP: " + enemyHP;
+    }
 }
+
+function magic() {
+  document.getElementById("message").textContent =
+    "You cast a spell!";
+}
+
+function defend() {
+  document.getElementById("message").textContent =
+    "You defend yourself!";
+}
+
+function useItem() {
+  document.getElementById("message").textContent =
+    "You used an item!";
+}
+    
